3c85b6e30ea94422487a29cc064e4aec2dbe857f
Sergey Havrik <s.gavrik83@gmail.com>
2022-05-30 21:20:02 +0300

    finished first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', startBtnHandler);
+stopBtn.addEventListener('click', stopBtnHandler);
+
+let timerId = null;
+let disablet = false;
+
+function startBtnHandler() {
+    if (disablet) {
+        return
+    };
+    disablet = true;
+    timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+};
+
+function stopBtnHandler() {
+    clearInterval(timerId);
+    disablet = false;
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
