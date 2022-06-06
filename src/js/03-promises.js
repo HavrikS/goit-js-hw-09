06e6d0f5e3e50b42daca7f7c86b551f352a3bd90
Sergey Havrik <s.gavrik83@gmail.com>
2022-06-06 12:15:32 +0300

    finished the third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const form = document.querySelector('.form');
+
+
+const onSubmit = (e) => {
+  e.preventDefault()
+  for (i = 1; i <= parseInt(amount.value); i += 1){
+    let position = i;
+    const newDelay = parseInt(delay.value) + parseInt(step.value) * (i - 1);
+    createPromise(position, newDelay)
+      .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  }
+  }
+
+form.addEventListener('submit', onSubmit)
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
   if (shouldResolve) {
-    // Fulfill
+    resolve({ position, delay });
   } else {
-    // Reject
+    reject({ position, delay });
   }
+      }, delay) 
+  })
+  return promise;
 }
+
+
